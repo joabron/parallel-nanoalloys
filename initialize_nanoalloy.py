17bccfd82999c3e9abaafb4d9946e367bdb2cdc1
PhilipVos <vosphilip@gmail.com>
2022-12-05 14:15:52 +0100

    small unusefull edits

--- initialize_nanoalloy.py
+++ initialize_nanoalloy.py
@@ -3,12 +3,22 @@
 # imports
 from ase.cluster.cubic import FaceCenteredCubic
 from ase.visualize import view
+import random as rd
 
-atom1 = "Ni"
+A = 5
+B = 5
+atom_A = "Ni"
+atom_B = "Fe"
 
 surfaces = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
 layers = [1, 1, 1]
 lc = 3.61 
-cluster = FaceCenteredCubic(atom1, surfaces, layers, lc)
+cluster = FaceCenteredCubic(atom_A, surfaces, layers, lc)
 
+for i in range(B):
+        index = rd.randint(0, A+B)
+        cluster[index].symbol = atom_B
+
+#print(cluster[0].symbol)
+view(cluster[0:4])
 view(cluster)

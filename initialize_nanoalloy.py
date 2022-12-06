ed152d035c663fbb11c6d4a52f2fddafc0c8a523
PhilipVos <vosphilip@gmail.com>
2022-12-06 11:26:15 +0100

    new functions and some comments

--- initialize_nanoalloy.py
+++ initialize_nanoalloy.py
@@ -11,16 +11,22 @@ This script contains functions for initializing nanoalloys in different ways.
 
 '''
 #Make a function to do num_A = ... and num_B = ... in rectangle
-#Make a function to build core-shell
-#Make a function to build layered cube
+#Make a function to build core-shell without hardcode
+#Make a function to build layered box
 #Make a function to build sub-clustered
 #Check if metals are fcc or bcc
 
-def build_random_cube(atom_A, atom_B, ratio, size):
+def build_random_box(atom_A, atom_B, ratio, x, y, z):
+    #The input must be odd numbers
+    #Sometimes a even number of rows is returned?
     surfaces = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
-    size = int((size - 1)/2)
-    layers = [size, size, size]
+    x = int((x - 1) / 2)
+    y = int((y - 1) / 2)
+    z = int((z - 1) / 2)
+    layers = [x, y, z]
     cluster = FaceCenteredCubic(atom_A, surfaces, layers)
+    
+    #swap atom_A for atom_B at random
     num_atoms = len(cluster)
     swap_atoms = int(ratio * num_atoms)
     i = 0
@@ -44,4 +50,13 @@ def build_random_sphere(atom_A, atom_B, ratio, size):
         if cluster[index].symbol != atom_B:
             cluster[index].symbol = atom_B
             i += 1
+    return cluster
+
+#A hardcoded function for a core of 2 layers and a shell of 1 layer
+def build_core2shell1(atom_A, atom_B):
+    surfaces = [(1, 1, 1), (1, 1, 0), (1, 0, 0)]
+    layers = [3, 3, 3]
+    cluster = FaceCenteredCubic(atom_A, surfaces, layers)
+    for i in [5, 8, 12, 13, 17, 21, 23, 26, 27, 29, 30, 31, 32]:
+        cluster[i].symbol = atom_B
     return cluster

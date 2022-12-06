d808aa184619f7ac456ea5c58fd4f80ec7ee2bed
PhilipVos <vosphilip@gmail.com>
2022-12-06 10:40:04 +0100

    added two funtions and to-do's

--- initialize_nanoalloy.py
+++ initialize_nanoalloy.py
@@ -2,23 +2,46 @@
 
 # imports
 from ase.cluster.cubic import FaceCenteredCubic
+from ase.cluster.cubic import BodyCenteredCubic
 from ase.visualize import view
 import random as rd
 
-A = 5
-B = 5
-atom_A = "Ni"
-atom_B = "Fe"
+'''
+This script contains functions for initializing nanoalloys in different ways. 
 
-surfaces = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
-layers = [1, 1, 1]
-lc = 3.61 
-cluster = FaceCenteredCubic(atom_A, surfaces, layers, lc)
+'''
+#Make a function to do num_A = ... and num_B = ... in rectangle
+#Make a function to build core-shell
+#Make a function to build layered cube
+#Make a function to build sub-clustered
+#Check if metals are fcc or bcc
 
-for i in range(B):
-        index = rd.randint(0, A+B)
-        cluster[index].symbol = atom_B
+def build_random_cube(atom_A, atom_B, ratio, size):
+    surfaces = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+    size = int((size - 1)/2)
+    layers = [size, size, size]
+    cluster = FaceCenteredCubic(atom_A, surfaces, layers)
+    num_atoms = len(cluster)
+    swap_atoms = int(ratio * num_atoms)
+    i = 0
+    while i < swap_atoms:
+        index = rd.randint(0, num_atoms - 1)
+        if cluster[index].symbol != atom_B:
+            cluster[index].symbol = atom_B
+            i += 1
+    return cluster
 
-#print(cluster[0].symbol)
-view(cluster[0:4])
-view(cluster)
+def build_random_sphere(atom_A, atom_B, ratio, size):
+    symbols = atom_A
+    surfaces = [(1, 1, 1), (1, 1, 0), (1, 0, 0)]
+    layers = [size-1, size, size-1]
+    cluster = FaceCenteredCubic(atom_A, surfaces, layers)
+    num_atoms = len(cluster)
+    swap_atoms = int(ratio * num_atoms)
+    i = 0
+    while i < swap_atoms:
+        index = rd.randint(0, num_atoms - 1)
+        if cluster[index].symbol != atom_B:
+            cluster[index].symbol = atom_B
+            i += 1
+    return cluster
